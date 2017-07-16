bb8caaa51ca3b15cd7bbba22223bad9543b38527
David Trenear <david@kimberlineducation.com.au>
2017-07-16 16:25:43 +1000

    Setup python file for application

--- start-game.bat
+++ start-game.bat
@@ -16,7 +16,7 @@ echo ACTIVATE PYTHON VIRTUALENV and START GAME
 echo.
 rem kivy test cmd /k "cd /d C:\Users\david\Documents\slotsim\Scripts\ & activate & cd /d C:\Users\david\Documents\slotsim\share\kivy-examples\demo\showcase\ & python main.py runserver"
 cmd /k "cd /d C:\Users\david\Documents\slotsim\Scripts\ & activate & cd /d C:\Users\david\Documents\slotsim\ & python main.py runserver"
-echo.
+
 pause
 echo -------------
 echo SLOTSIM STARTING
