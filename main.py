10513e4ad801f2ab04d2786f8a5593f6c29daff7
David Trenear <david@kimberlineducation.com.au>
2017-07-22 09:19:49 +1000

    Setup kivy log control options, setup Kivy configuration control and options, protype for the game options in json format, setup to control kivy home directory so can set game dirrectory and later switch

--- main.py
+++ main.py
@@ -5,8 +5,36 @@ n = ''
 print(s)
 print(n)
 
-# Python library import
+# SET KIVY HOME TO BE THE GAME FOLDER
+import os
+os.environ['KIVY_HOME'] = 'Games\Default\\'
+print('SET KIVY HOME')
+print(os.environ['KIVY_HOME'])
 
+# SET KIVY CONFIG OPTIONS
+from kivy.config import Config
+# window_state: string , one of ‘visible’, ‘’, ‘maximized’
+Config.set('graphics', 'window_state', 'visible')
+
+# TURN ON OR OFF LOG FILES AND TUNE VERBOCITY
+# Differents logging levels are available : trace, debug, info, warning, error and critical.
+# Config.set('kivy', 'log_level', 'trace')
+# log_enable = 1
+Config.set('kivy', 'log_enable', 0)
+
+Config.write()
+
+# SET GAME CONFIG OPTIONS
+import json
+print(json.dumps({'GameName': 'Slot Sim', 'Version': "0.1"}, sort_keys=True, indent=4))
+
+
+# Library Import
+
+# PYTHON
+from random import random
+
+# Kivy
 from kivy.app import App
 from kivy.uix.widget import Widget
 from kivy.uix.boxlayout import BoxLayout
@@ -16,17 +44,17 @@ from kivy.logger import Logger
 from kivy.event import EventDispatcher
 from kivy.uix.screenmanager import ScreenManager, Screen
 from kivy.lang import Builder
-from random import random
 
-
-#Load the KV file
+# Load the KV file for the game
 Interface = Builder.load_file("slotsim.kv")
+
 
 # Class declaration ?????????????????????????????????????????????????????????????????
 
 # Declare screen classes
 class GameScreen(Screen):
     pass
+
 
 class SettingsScreen(Screen):
     pass
@@ -40,6 +68,7 @@ ScreenSystem = ScreenManager()
 # Add screens to screen manager
 ScreenSystem.add_widget(GameScreen(name='GameScreen'))
 ScreenSystem.add_widget(SettingsScreen(name='SettingsScreen'))
+
 
 # Application loop @@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
 
@@ -56,76 +85,77 @@ class SlotsimApp(App):
     def on_stop(self):
         Logger.critical('App: Aaaargh I\'m dying!')
 
+
 # Application run
 if __name__ == '__main__':
     SlotsimApp().run()
 
 
-#Examples before implimentation if you know what I mean!
+    # Examples before implimentation if you know what I mean!
 
-# <DrawingWidget>:
-#     canvas:
-#         Color:
-#             rgba: 1, 1, 1, 1
-#         Rectangle:
-#             pos: self.pos
-#             size: self.size
-#             #size: (50, 50)
-#
-# <ColourSlider@Slider>:
-#     min: 0
-#     max: 1
-#     value: 0.5
-#     size_hint_y: None
-#     height: 80
+    # <DrawingWidget>:
+    #     canvas:
+    #         Color:
+    #             rgba: 1, 1, 1, 1
+    #         Rectangle:
+    #             pos: self.pos
+    #             size: self.size
+    #             #size: (50, 50)
+    #
+    # <ColourSlider@Slider>:
+    #     min: 0
+    #     max: 1
+    #     value: 0.5
+    #     size_hint_y: None
+    #     height: 80
 
-# <Interface>:
-#     orientation: 'vertical'
-#     Button:
-#         Image:
-#             source: 'Games/Default/Images/Button_Base_Oval_Normal.png'
-#             #y: self.parent.y + self.parent.height - 250
-#             #x: self.parent.x
-#             center_x: self.parent.center_x
-#             center_y: self.parent.center_y
-#             size: 250, 250
-#             allow_stretch: True
-#         Label:
-#             text: "Spin"
-#     Label:
-#         text: "Spin"
+    # <Interface>:
+    #     orientation: 'vertical'
+    #     Button:
+    #         Image:
+    #             source: 'Games/Default/Images/Button_Base_Oval_Normal.png'
+    #             #y: self.parent.y + self.parent.height - 250
+    #             #x: self.parent.x
+    #             center_x: self.parent.center_x
+    #             center_y: self.parent.center_y
+    #             size: 250, 250
+    #             allow_stretch: True
+    #         Label:
+    #             text: "Spin"
+    #     Label:
+    #         text: "Spin"
 
-	#	background_color: 80,50,40,0.5
-	#	size: 100,75
-	#	pos: 0,0
-	#	text: str(self.state)
-	#	color: 20,100,0,1
-	#	font_size: 40
+    #	background_color: 80,50,40,0.5
+    #	size: 100,75
+    #	pos: 0,0
+    #	text: str(self.state)
+    #	color: 20,100,0,1
+    #	font_size: 40
 
-    #BoxLayout:
+    # BoxLayout:
     #    orientation: 'horizontal'
     #    size_hint_y: None
     #    height: 140
     #    Label:
     #        text: 'output colour:'
 
-# class DrawingWidget(Widget):
-#
-#     def on_touch_down(self, touch):
-#         super(DrawingWidget, self).on_touch_down(touch)
-#
-#         if not self.collide_point(*touch.pos):
-#             return
-#
-#         with self.canvas:
-#             Color(random(), random(), random())
-#             self.line = Line(points=[touch.pos[0], touch.pos[1]], width=2)
-#
-#     def on_touch_move(self, touch):
-#         if not self.collide_point(*touch.pos):
-#             return
-#
-#         self.line.points = self.line.points + [touch.pos[0], touch.pos[1]]
+    # class DrawingWidget(Widget):
+    #
+    #     def on_touch_down(self, touch):
+    #         super(DrawingWidget, self).on_touch_down(touch)
+    #
+    #         if not self.collide_point(*touch.pos):
+    #             return
+    #
+    #         with self.canvas:
+    #             Color(random(), random(), random())
+    #             self.line = Line(points=[touch.pos[0], touch.pos[1]], width=2)
+    #
+    #     def on_touch_move(self, touch):
+    #         if not self.collide_point(*touch.pos):
+    #             return
+    #
+    #         self.line.points = self.line.points + [touch.pos[0], touch.pos[1]]
 
-# class Interface(BoxLayout):
-#     pass
+    # class Interface(BoxLayout):
+    #     pass
